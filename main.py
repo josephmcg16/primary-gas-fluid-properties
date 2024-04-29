fa3d7218e820d73b13d263510cf7eeef66b4c519
josephmcg16 <joseph.mcgovern16@googlemail.com>
2024-04-29 22:12:43 +0100

    Add isentropic exponent to main.py and new functions

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 """Script to fit gas viscosity and density models to reference data and save the best models as .txt files to disk."""
+
+import re
 import pandas as pd
 import numpy as np
-import plotly.express as px
-from tqdm import tqdm
 
-from fit_fluid_properties import fit_gas_density, fit_gas_viscosity
+from fit_fluid_properties import (
+    fit_gas_density,
+    fit_gas_viscosity,
+    fit_isentropic_exponent,
+)
 
 
 CONFIG_LIST = [
@@ -25,14 +29,47 @@ CONFIG_LIST = [
     },
 ]
 
-for config in CONFIG_LIST:
+
+def fit_fluid_property_models(
+    reference_data_directory: str,
+    reference_data_filename: str,
+    reference_fluid: str,
+    model_poly_degree_range: np.ndarray = np.arange(1, 11, 1),
+) -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    """
+    Fit the best models to the reference data and return the best models for viscosity, density, and isentropic exponent.
+
+    Parameters
+    ----------
+    reference_data_directory: str
+        The directory containing the reference data.
+    reference_data_filename: str
+        The filename of the reference data.
+    reference_fluid: str
+        The reference fluid to be used for the model fitting.
+    model_poly_degree_range: np.ndarray
+        The range of polynomial degrees to be searched for model selection.
+
+    Returns
+    -------
+    tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]
+        A tuple containing the DataFrames with the results for viscosity, density, and isentropic exponent models.
+    """
+    config = {
+        "reference_data_directory": reference_data_directory,
+        "reference_data_filename": reference_data_filename,
+        "reference_fluid": reference_fluid,
+    }
     viscosity_fits = []
     density_fits = []
-    for model_poly_degree in tqdm(np.arange(1, 11, 1), desc="Model Poly Degree"):
+    isentropic_exponent_fits = []
+    for model_poly_degree in model_poly_degree_range:
         config["model_poly_degree"] = model_poly_degree
         df_viscosity, viscosity_model_functional_form_text = fit_gas_viscosity(config)
         df_density, density_model_functional_form_text = fit_gas_density(config)
-
+        df_isentropic_exponent, isentropic_exponent_model_functional_form_text = (
+            fit_isentropic_exponent(config)
+        )
         viscosity_fits.append(
             {
                 "model_poly_degree": model_poly_degree,
@@ -62,30 +99,98 @@ for config in CONFIG_LIST:
                 "functional_form_text": density_model_functional_form_text,
             }
         )
+        isentropic_exponent_fits.append(
+            {
+                "model_poly_degree": model_poly_degree,
+                "df": df_isentropic_exponent,
+                "Isentropic Exponent Relative Error Max (%)": df_isentropic_exponent[
+                    "Isentropic Exponent Relative Error (%)"
+                ].max(),
+                "Isentropic Exponent Relative Error CI (%)": 1.96
+                * df_isentropic_exponent["Isentropic Exponent Relative Error (%)"].std()
+                / np.sqrt(len(df_isentropic_exponent)),
+                "functional_form_text": isentropic_exponent_model_functional_form_text,
+            }
+        )
+    return (
+        pd.DataFrame(viscosity_fits),
+        pd.DataFrame(density_fits),
+        pd.DataFrame(isentropic_exponent_fits),
+    )
 
-    viscosity_fits_df = pd.DataFrame(viscosity_fits)
-    density_fits_df = pd.DataFrame(density_fits)
 
-    viscosity_best_model_idx = viscosity_fits_df[
-        "Viscosity Relative Error Max (%)"
-    ].idxmin()
-    viscosity_best_model = viscosity_fits_df.loc[viscosity_best_model_idx]
+def get_best_model(df: pd.DataFrame, error_column: str) -> pd.DataFrame:
+    """
+    Return the best model from the DataFrame based on the minimum error.
 
-    density_best_model_idx = density_fits_df["Density Relative Error Max (%)"].idxmin()
-    density_best_model = density_fits_df.loc[density_best_model_idx]
+    Parameters
+    ----------
+    df: pd.DataFrame
+        The DataFrame containing the model results.
+    error_column: str
+        The column containing the error to be minimized.
 
-    with open(
-        f'models/Viscosity Model {config["reference_data_filename"].replace(".xlsx", ".txt")}',
-        "w",
-        encoding="utf-8",
-    ) as file:
-        file.write(viscosity_best_model["functional_form_text"])
-        file.write(f"\n{viscosity_best_model.df.describe()}")
+    Returns
+    -------
+    pd.DataFrame
+        The best model based on the minimum error.
+    """
+    best_model_idx = df[error_column].idxmin()
+    return df.loc[best_model_idx]
 
-    with open(
-        f'models/Density Model {config["reference_data_filename"].replace(".xlsx", ".txt")}',
-        "w",
-        encoding="utf-8",
-    ) as file:
-        file.write(density_best_model["functional_form_text"])
-        file.write(f"\n{density_best_model.df.describe()}")
+
+def save_best_model_to_disk(best_model: pd.DataFrame, model_type: str, filename: str):
+    """
+    Save the best model to disk as a .txt file and the model DataFrame as a .h5 file.
+
+    Parameters
+    ----------
+    best_model: pd.DataFrame
+        The best model to be saved.
+    model_type: str
+        The type of model being saved.
+    filename: str
+        The filename to save the model as.
+    """
+    with open(f"models/{model_type} Model {filename}.txt", "w", encoding="utf-8") as file:
+        file.write(best_model["functional_form_text"])
+        file.write(f"\n{best_model.df.describe()}")
+
+    best_model.to_pickle(f"models/{model_type} Model {filename}.pkl")
+
+
+if __name__ == "__main__":
+    for config in CONFIG_LIST:
+        viscosity_fits_df, density_fits_df, isentropic_exponent_fits_df = (
+            fit_fluid_property_models(
+                reference_data_directory=config["reference_data_directory"],
+                reference_data_filename=config["reference_data_filename"],
+                reference_fluid=config["reference_fluid"],
+            )
+        )
+
+        viscosity_best_model = get_best_model(
+            viscosity_fits_df, "Viscosity Relative Error Max (%)"
+        )
+        density_best_model = get_best_model(
+            density_fits_df, "Density Relative Error Max (%)"
+        )
+        isentropic_exponent_best_model = get_best_model(
+            isentropic_exponent_fits_df, "Isentropic Exponent Relative Error Max (%)"
+        )
+
+        save_best_model_to_disk(
+            viscosity_best_model,
+            "Viscosity",
+            config["reference_data_filename"].replace(".xlsx", ""),
+        )
+        save_best_model_to_disk(
+            density_best_model,
+            "Density",
+            config["reference_data_filename"].replace(".xlsx", ""),
+        )
+        save_best_model_to_disk(
+            isentropic_exponent_best_model,
+            "Isentropic Exponent",
+            config["reference_data_filename"].replace(".xlsx", ""),
+        )
