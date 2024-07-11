ecedab1bd64708ba8e7f2d0d0736911f9deb5d6a
Joseph McGovern <mcgov-jo@gb010.itgr.net>
2024-07-11 12:18:08 +0100

    Further refactor

--- generate_data.py
+++ generate_data.py
@@ -1,3 +1,4 @@
+"""Generate fluid properties for each sample in a design of experiments (DOE)."""
 import pandas as pd
 from tqdm import tqdm
 from refprop.utils import generate_temperature_pressure_samples
@@ -5,6 +6,7 @@ from refprop.refprop import RefpropInterface
 
 
 def rename_columns(df:pd.DataFrame):
+    """Rename columns to more descriptive names for scikit-learn modules."""
     df["Temperature (C)"] = df["T"] - 273.15  # K to C
     df["Pressure (bar)"] = df["P"] / 1e5  # Pa to bar
     df.drop(columns=["T", "P"], inplace=True)
@@ -18,11 +20,32 @@ def rename_columns(df:pd.DataFrame):
     return df
 
 
-def get_fluid_property(doe:pd.DataFrame, hFld:str, hOut:str, z:list, show_progress:bool=False):
+def get_fluid_property(doe:pd.DataFrame, hFld:str, hOut:str, z:list, show_progress:bool=False, rename:bool=False):
+    """Get fluid properties for each sample in the doe.
+    
+    Args
+    ----
+    doe: pd.DataFrame
+        DataFrame containing the samples to be evaluated.
+    hFld: str
+        Path to the fluid file(s). E.g., 'refprop/FLUIDS/PROPANE.FLD;refprop/FLUIDS/ETHANE.FLD'
+    hOut: str
+        Output properties to be calculated. E.g., 'Z,D,VIS,ISENK,CSTAR'
+    z: list
+        Composition array for each fluid in hFld.
+    show_progress: bool
+        Show tqdm progress bar.
+    
+    Returns
+    -------
+    pd.DataFrame
+        DataFrame containing the fluid properties for each sample in the
+        doe.
+    """
     if show_progress:
         progress_bar = tqdm
     else:
-        progress_bar = lambda x: x
+        progress_bar = lambda x, total: x
     refprop = RefpropInterface(r"T:\Joseph McGovern\Code\GitHub\refprop-dotnet\refprop", hFld)
     df = pd.DataFrame()
     for index, sample in progress_bar(doe.iterrows(), total=len(doe)):
@@ -37,12 +60,15 @@ def get_fluid_property(doe:pd.DataFrame, hFld:str, hOut:str, z:list, show_progre
             z  # composition array
         )
         df = pd.concat([df, pd.DataFrame(refprop_output, index=[index])])
+
     df["T"] = doe["Temperature [K]"]
     df["P"] = doe["Pressure [Pa]"]
-    df = rename_columns(df)
-    cols = df.columns.tolist()
-    cols = cols[-2:] + cols[:-2]
-    df = df[cols]
+
+    if rename:
+        df = rename_columns(df)
+        cols = df.columns.tolist()
+        cols = cols[-2:] + cols[:-2]
+        df = df[cols]
     return df
 
 
@@ -55,6 +81,7 @@ if __name__ == "__main__":
     df_dict = {}
     for fluid, config in df_config.iterrows():
         print(f"Generating data for {fluid}")
+       
         df_dict[fluid] = get_fluid_property(
             doe=generate_temperature_pressure_samples(
                 n_grid_temperature=int(config["N_GRID_TEMPERATURE"]),
@@ -65,6 +92,7 @@ if __name__ == "__main__":
             hFld=f"refprop/FLUIDS/{fluid}.FLD",
             hOut="Z,D,VIS,ISENK,CSTAR",
             z=[1.0] + [0.0] * 19,
-            show_progress=True
+            show_progress=True,
+            rename=True
         )
         df_dict[fluid].to_excel(f"data\\{fluid} PROPERTIES.xlsx", index=False)
--- impurities.py
+++ impurities.py
@@ -1,156 +1,149 @@
+"""Script to calculate the impact of impurities on fluid properties calculations."""
 import pandas as pd
 import numpy as np
-
 import yaml
 
-from tqdm import tqdm
-
-from refprop.refprop import RefpropInterface
 from refprop.utils import generate_temperature_pressure_samples
+from generate_data import get_fluid_property
 
 
-DLL_PATH = r"T:\Joseph McGovern\Code\GitHub\refprop-dotnet\refprop"
-
-
-def get_fluid_property(doe: pd.DataFrame, hFld: str, hOut: str, z: list, composition_basis: str = "mol", show_progress: bool = False):
-    if show_progress:
-        progress_bar = tqdm
-    else:
-        progress_bar = lambda x, total: x
-    refprop = RefpropInterface(DLL_PATH, hFld)
-    df = pd.DataFrame()
-    for index, sample in progress_bar(doe.iterrows(), total=len(doe)):
-        if composition_basis == "vol":
-            pass
-            # TODO: Convert vol/vol to mol/mol
-
-        refprop_output = refprop.refprop2dll(
-            hFld,
-            "TP",  # Input string of properties (Temperature and Pressure)
-            hOut,  # Output properties to be calculated
-            21,  # mass base SI units
-            0,
-            sample["Temperature [K]"],  # Temperature in K
-            sample["Pressure [Pa]"],  # Pressure in Pa
-            z  # composition array, mol/mol or vol/vol
-        )
-        df = pd.concat([df, pd.DataFrame(refprop_output, index=[index])])
-    df["T"] = doe["Temperature [K]"]
-    df["P"] = doe["Pressure [Pa]"]
-    return df
-
-
-def get_impurities_properties(fluids: list, doe: pd.DataFrame, df_ref: pd.DataFrame, fluid_property:str, composition_basis, show_progress:bool=False):
+def get_impurities_properties(
+        fluids: list, doe: pd.DataFrame, df_ref: pd.DataFrame, fluid_property:str, show_progress:bool=False
+    ):
+    """Get fluid properties df for each impurities in the fluids list."""
     df_dict = {}
     for fluid in fluids:
-        print(fluid) if show_progress else None
-        df_dict[fluid] = get_fluid_property(doe, f"refprop/FLUIDS/{fluid}", fluid_property, [1.0] + [0.0] * 19, composition_basis, show_progress=show_progress)
-        df_dict[fluid][f"{fluid_property}_Err"] = (df_dict[fluid][fluid_property] - df_ref[fluid_property])
+        if show_progress:
+            print(fluid)
+        df_dict[fluid] = get_fluid_property(
+            doe=doe,
+            hFld=f"refprop/FLUIDS/{fluid}",
+            hOut=fluid_property,
+            z=[1.0] + [0.0] * 19,
+            show_progress=show_progress
+        )
+        df_dict[fluid][f"{fluid_property}_Err"] = (
+            df_dict[fluid][fluid_property] - df_ref[fluid_property]
+        )
     return df_dict
 
 
 def get_impact_metrics(df_impurities: pd.DataFrame, fluid_property: str):
+    """Integrate fluid properties error between impurities and pure fluid over operating range."""
     integral_series = pd.Series({
         fluid: np.trapz(
             y=np.trapz(
-                y=df_impurities[fluid].pivot(index="T", columns="P", values=f"{fluid_property}_Err"),
+                y=df_impurities[fluid].pivot(
+                    index="T", columns="P", values=f"{fluid_property}_Err"
+                ),
                 x=df_impurities[fluid]["T"].unique()
             ),
             x=df_impurities[fluid]["P"].unique()
         ) for fluid in df_impurities
-    }, name="Integral of Density Deviation over Temperature and Pressure Range")
+    }, name=f"Integral of {fluid_property} Deviation over Temperature and Pressure Range")
 
     sorted_series = integral_series.reindex(integral_series.abs().sort_values(ascending=False).index)
     return sorted_series
 
 
-def normalize_impurities(impurities, impurities_upper_limits, base_compositions_lower_limits, normalized=None, current_sum=0.0):
+def normalize_impurities(
+        impurities,
+        impurities_upper_limits,
+        base_compositions_lower_limits,
+        normalized=None,
+        current_sum=0.0
+    ) -> pd.Series:
+    """Sort impurities by impact metrics and normalize them to the worst-case scenario 
+    (i.e., total composition of 100%)."""
     if normalized is None:
         normalized = {}
 
     # Calculate target sum from the base compositions lower limits
     target = 1 - pd.Series(base_compositions_lower_limits).sum()
-    
+
     if impurities.empty or current_sum >= target:
         normalized.update(base_compositions_lower_limits)
         return pd.Series(normalized, name="Normalized Worst-Case Compositions")
 
     impurity = impurities.index[0]
     limit = impurities_upper_limits[impurity]
-    
+
     if current_sum + limit <= target:
         normalized[impurity] = limit
-        return normalize_impurities(impurities[1:], impurities_upper_limits, base_compositions_lower_limits, normalized, current_sum + limit)
-    else:
-        remaining_limit = target - current_sum
-        if remaining_limit > 0:
-            normalized[impurity] = remaining_limit
-        normalized.update(base_compositions_lower_limits)
-        return pd.Series(normalized, name="Normalized Worst-Case Compositions")
+        return normalize_impurities(
+            impurities[1:],
+            impurities_upper_limits,
+            base_compositions_lower_limits,
+            normalized,
+            current_sum + limit
+        )
+    remaining_limit = target - current_sum
+    if remaining_limit > 0:
+        normalized[impurity] = remaining_limit
+    normalized.update(base_compositions_lower_limits)
+    return pd.Series(normalized, name="Normalized Worst-Case Compositions")
 
 
 if __name__ == "__main__":
-    import plotly.graph_objects as go
-    import plotly.express as px
+    def main():
+        """Main function to run the script."""
+        with open('config\\CO2_IMPURITIES.yaml', 'r', encoding='utf-8') as file:
+            config = yaml.safe_load(file)
 
-    with open('config\\CO2_IMPURITIES.yaml', 'r', encoding='utf-8') as file:
-        config = yaml.safe_load(file)
+        base_compositions_lower_limits = pd.Series(
+            config['BASE_COMPOSITIONS_LOWER_LIMITS'], 
+            name="Base Compositions [mol/mol]"
+        )
+        impurities_upper_limits = pd.Series(
+            config['IMPURITIES_UPPER_LIMITS'],
+            name="Impurities Upper Limits [mol/mol]"
+        )
 
-    BASE_COMPOSITIONS_LOWER_LIMITS = pd.Series(config['BASE_COMPOSITIONS_LOWER_LIMITS'], name="Base Compositions [mol/mol]")
-    IMPURITIES_UPPER_LIMITS = pd.Series(config['IMPURITIES_UPPER_LIMITS'], name="Impurities Upper Limits [mol/mol]")
+        doe = generate_temperature_pressure_samples(
+            config["DOE"]["n_grid_temperature"],
+            config["DOE"]["n_grid_pressure"],
+            config["DOE"]["temperature_range"],
+            config["DOE"]["pressure_range"],
+        )
 
-    DOE = generate_temperature_pressure_samples(
-        config["DOE"]["n_grid_temperature"],  # n_grid_temperature
-        config["DOE"]["n_grid_pressure"],  # n_grid_pressure
-        config["DOE"]["temperature_range"],  # temperature_range
-        config["DOE"]["pressure_range"],  # pressure_range
-    )
+        df_ref = get_fluid_property(
+            doe=doe,
+            hFld=";".join(
+                base_compositions_lower_limits.add_prefix("refprop/FLUIDS/").index
+                ),
+            hOut=config['FLUID_PROPERTY'],
+            z=list(base_compositions_lower_limits.values) + \
+                [0.0] * (20 - len(base_compositions_lower_limits)), 
+        )
 
-    df_ref = get_fluid_property(
-        doe=DOE,
-        hFld=";".join(
-            BASE_COMPOSITIONS_LOWER_LIMITS.add_prefix("refprop/FLUIDS/").index
-        ),  # input fluids
-       hOut= config['FLUID_PROPERTY'],  # output fluid property
-        z=list(BASE_COMPOSITIONS_LOWER_LIMITS.values) + \
-            [0.0] * (20 - len(BASE_COMPOSITIONS_LOWER_LIMITS)),  # composition array,
-        composition_basis=config['COMPOSITIONS_BASIS'],    
-    )
-    df_impurities = get_impurities_properties(
-        fluids=list(IMPURITIES_UPPER_LIMITS.keys()),
-        doe=DOE,
-        df_ref=df_ref,
-        fluid_property=config['FLUID_PROPERTY'],
-        composition_basis=config['COMPOSITIONS_BASIS']
-    )
+        df_impurities = get_impurities_properties(
+            fluids=list(impurities_upper_limits.keys()),
+            doe=doe,
+            df_ref=df_ref,
+            fluid_property=config['FLUID_PROPERTY'],
+        )
 
-    impact_metrics_density = get_impact_metrics(df_impurities, config['FLUID_PROPERTY'])
-    compositions_worst_case = normalize_impurities(
-        impurities=impact_metrics_density,
-        impurities_upper_limits=IMPURITIES_UPPER_LIMITS,
-        base_compositions_lower_limits=BASE_COMPOSITIONS_LOWER_LIMITS
-    )
+        impact_metrics_density = get_impact_metrics(df_impurities, config['FLUID_PROPERTY'])
+        compositions_worst_case = normalize_impurities(
+            impurities=impact_metrics_density,
+            impurities_upper_limits=impurities_upper_limits,
+            base_compositions_lower_limits=base_compositions_lower_limits
+        )
 
-    df_worst_case = get_fluid_property(
-        doe=DOE,
-        hFld=";".join(compositions_worst_case.add_prefix("refprop/FLUIDS/").index), 
-        hOut=config['FLUID_PROPERTY'], 
-        z=list(compositions_worst_case.values) + [0.0] * (20 - compositions_worst_case.shape[0]),
-        composition_basis=config['COMPOSITIONS_BASIS'],
-    )
-    df_worst_case["Density Deviation [%]"] = ((df_worst_case.D - df_ref.D) / df_ref.D * 100)
-    
-    print(impact_metrics_density)
-    print(compositions_worst_case)
-    print(compositions_worst_case.sum())
-    print(df_worst_case["Density Deviation [%]"].describe())
+        df_worst_case = get_fluid_property(
+            doe=doe,
+            hFld=";".join(compositions_worst_case.add_prefix("refprop/FLUIDS/").index),
+            hOut=config['FLUID_PROPERTY'],
+            z=list(compositions_worst_case.values) + \
+                [0.0] * (20 - compositions_worst_case.shape[0]),
+        )
+        deviation_col = f"{config['FLUID_PROPERTY']} Deviation [%]"
+        df_worst_case[deviation_col] = (df_worst_case.D - df_ref.D) / df_ref.D * 100.0
 
-    fig = px.imshow(
-        df_worst_case.pivot(index="T", columns="P", values="Density Deviation [%]"),
-        labels=dict(x="Pressure [Pa]", y="Temperature [K]", color="Density Deviation [%]"),
-        title=f"METHANE Density Deviation [%] Worst Case Scenario",
-        x=df_worst_case["P"].unique(),
-        y=df_worst_case["T"].unique(),
-        aspect="auto"
-    )
-    fig.show()
+        print(f"Impact Metrics: \n{impact_metrics_density}\n{'-' * 80}")
+        print(f"Worst-Case Compositions: \n{compositions_worst_case}\n{'-' * 80}")
+        print(f"Worst-Case Sum: {compositions_worst_case.sum()}\n{'-' * 80}")
+        print(f"Worst-Case Deviation Metrics: \n"
+              f"{df_worst_case[deviation_col].describe()}")
+
+    main()
